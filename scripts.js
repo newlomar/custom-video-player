63a552ec8252cfa97041b781cdc56f76e116eaa0
Newton Lomar <newtonlomar16@gmail.com>
2021-10-14 22:04:12 -0300

    function to change video status created and used for video click and play button click

---
+++ scripts.js
@@ -0,0 +1,28 @@
+/* Selecionar os elementos */
+
+const video = document.querySelector("[data-js='video']");
+const playButton = document.querySelector("[data-js='playButton']");
+const progressBar = document.querySelector("[data-js='progressBar'");
+const volumeBar = document.querySelector("[data-js='volumeBar']");
+const speedBar = document.querySelector("[data-js='speedBar']");
+const skipLess10 = document.querySelector("[data-skip='-10']");
+const skipPlus25 = document.querySelector("[data-skip='25']");
+
+/* Criar as funções */
+
+function alterVideoStatus() {
+
+  if (video.paused) {
+    video.play()
+  }
+
+  else {
+    video.pause()
+  }
+
+}
+
+/* Conectar com os events Listeners */
+
+video.addEventListener('click', alterVideoStatus)
+playButton.addEventListener('click', alterVideoStatus)
